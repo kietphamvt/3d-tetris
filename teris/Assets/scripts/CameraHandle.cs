08c1ff7e8919aeb36f705612b43ba1dc78e3dcaf
SuckTinHock <paulpham.kiet@gmail.com>
2021-06-23 18:05:11 +0700

    Update

--- teris/Assets/scripts/CameraHandle.cs
+++ teris/Assets/scripts/CameraHandle.cs
@@ -8,8 +8,8 @@ public class CameraHandle : MonoBehaviour
     private Vector3 previousPosition;
     void Start()
     {
-        cam.transform.localPosition = new Vector3(0f, 31f, -31.8f);
-        cam.transform.localEulerAngles = new Vector3(30f, 0f, 0f);
+        cam.transform.localPosition = new Vector3(0f, 25f, -31.8f);
+        cam.transform.localEulerAngles = new Vector3(20f, 0f, 0f);
     }
 
     
@@ -23,11 +23,11 @@ public class CameraHandle : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             Vector3 direction = previousPosition - cam.ScreenToViewportPoint(Input.mousePosition);
-            cam.transform.RotateAround(new Vector3(0, 31, 0), new Vector3(0, 1, 0), -direction.x * 180);    //Xoay theo Oy
+            cam.transform.RotateAround(new Vector3(0, 25, 0), new Vector3(0, 1, 0), -direction.x * 180);    //Xoay theo Oy
             Vector3 tmp = cam.transform.localEulerAngles;
-            tmp.x += direction.y * 180;
-            if (tmp.x < 0) tmp.x = 0;
-            else if (tmp.x > 40) tmp.x = 40;
+            tmp.x = (float)tmp.x + direction.y * 180;
+            if (tmp.x < 0) tmp.x = 0f;
+            else if (tmp.x > 40f) tmp.x = 40f;
             cam.transform.localEulerAngles = tmp;                                                           //Xoay theo Ox
             previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
         }    
