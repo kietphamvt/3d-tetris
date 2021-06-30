ecd6d843eef5599fdc7ab2b9f2abb3228d07cdeb
legoeuro <68761938+legoeruro@users.noreply.github.com>
2021-06-30 11:52:48 +0700

    chào
    mới dậy mà cũng bắt commit z ác quá
    code trong boxscript còn rất dơ và có thể chỉnh lại nhiều cách khác để đỡ dơ nha nhma dơ z thì check collision lại dễ nên cứ tự nhiên
    địt

--- teris/Assets/scripts/boxScript.cs
+++ teris/Assets/scripts/boxScript.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 public class boxScript : MonoBehaviour
 {
     GameObject blocc;
+    List<GameObject> OldBloccs = new List<GameObject>();
     Stopwatch watchu = new Stopwatch();
-    public LayerMask playahmask;
+    
+    //Instantiated GameObjects are spawned on default so we dont check collide on default but check on every other layers
+    public LayerMask notDefault;
+    public GameObject OldBloccFolder;
+
     int ren;
-    float increm = 2f;
-    int runtiem = 1000;
+    float increm = 2.1f;
+    int runtiem = 250;
     void Start()
     {
         spawnBlock();
@@ -24,8 +29,8 @@ public class boxScript : MonoBehaviour
         --> 2.14^3
         Height khung = 38.19 --> 18 cuc 
         */
-        ren = Random.Range(1,12);
-        blocc = Instantiate(Resources.Load("blocks/" + (ren).ToString())) as GameObject;
+        ren = Random.Range(1,10);
+        blocc = Instantiate(Resources.Load("New-Block/" + (ren).ToString())) as GameObject;
         blocc.transform.position = new Vector3(0.8f, 40, -0.99f);
     }
 
@@ -41,15 +46,57 @@ public class boxScript : MonoBehaviour
         {
             watchu.Stop(); watchu = new Stopwatch(); watchu.Start();
 
-            blocc.transform.position -= new Vector3(0, increm, 0);
 
-            if (Physics.OverlapSphere(blocc.transform.position, 0.25f).Length > 0)
+            if (FallDownCollide(blocc))
             {
-                blocc.AddComponent(typeof(BoxCollider));
-                BoxCollider hehe = blocc.GetComponent(typeof(BoxCollider)) as BoxCollider;
-                hehe.isTrigger = true;
+                int n = blocc.transform.childCount;
+                Transform child = blocc.transform.Find("Cube");
+                child.parent = OldBloccFolder.transform;
+                child.gameObject.layer = 6;
+                child.gameObject.AddComponent(typeof(BoxCollider));
+                OldBloccs.Add(child.gameObject);
+
+                for (int i = 1; i < n; ++i)
+                {
+                    child = blocc.transform.Find("Cube.00" + i.ToString());
+                    child.parent = OldBloccFolder.transform;
+                    child.gameObject.layer = 6;
+                    child.gameObject.AddComponent(typeof(BoxCollider));
+                    OldBloccs.Add(child.gameObject);
+                }
+                print("new blocks");
                 spawnBlock();
             }
+            else
+            {
+                blocc.transform.position -= new Vector3(0, increm, 0);
+            }
         }
+    }
+
+    private bool FallDownCollide(GameObject blocc)
+    {
+        GameObject tblocc = Instantiate(blocc) as GameObject;
+        tblocc.transform.position -= new Vector3(0, increm, 0);
+
+        print("fall down collide");
+        bool res = CheckCollision(tblocc);
+        Destroy(tblocc);
+        return res;
+    }
+
+    bool CheckCollision(GameObject blocc)
+    {
+        int n = blocc.transform.childCount;
+        Transform child = blocc.transform.Find("Cube");
+        if (Physics.OverlapBox(child.position, child.localScale, Quaternion.identity).Length > 0) return true;
+
+        for (int i = 1; i<n; ++i)
+        {
+            child = blocc.transform.Find("Cube.00" + i.ToString());
+            if (Physics.OverlapBox(child.position, child.localScale, Quaternion.identity).Length > 0) return true;
+        }
+
+        return false;
     }
 }
